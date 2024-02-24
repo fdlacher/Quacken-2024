4cdfbca74261b8998962195fc68366efeb564822
fdlacher <finndlacher@gmail.com>
2024-02-24 11:47:08 -0600

    Added scoring constants and updated shooterSubsystem
    remember to update constants.java before testing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -32,7 +32,15 @@ public final class Constants {
         public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
         public static final double kPTurning = 0.5;
     }
+    public static final class ScoringConstants{ 
+        public static final int kDirectionalIntakeMotorPort = 0;
+        public static final int kIntakeMotorPort = 0;
 
+        public static final int kShootSetMotor = 0;
+        public static final int kRightShootGoalMotor1 = 0;
+        public static final int kLeftShootGoalMotor = 0;
+
+    }
     public static final class DriveConstants {
 
         public static final double kTrackWidth = Units.inchesToMeters(21);
