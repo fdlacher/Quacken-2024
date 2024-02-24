bdd9d02f330dd119c4bcb1979ae4b1ef3189a079
Bryan Kitzrow <bkitzrow@francistuttle.edu>
2024-02-24 10:03:34 -0600

    theoretical intake

---
+++ src/main/java/frc/robot/subsystems/intakeSubsystem.java
@@ -0,0 +1,47 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class intakeSubsystem extends SubsystemBase{
+    private final CANSparkMax directionalIntakeMotor;
+    private final CANSparkMax intakeMotor;
+
+
+    public intakeSubsystem(int directionalIntakeMotorPort, int intakeMotorPort,boolean directionalIntakeMotorReversed, boolean intakeMotorReversed){
+        directionalIntakeMotor = new CANSparkMax(directionalIntakeMotorPort, MotorType.kBrushless);
+        intakeMotor = new CANSparkMax(intakeMotorPort, MotorType.kBrushless);
+
+        directionalIntakeMotor.setInverted(directionalIntakeMotorReversed);
+        intakeMotor.setInverted(intakeMotorReversed);
+    }
+    public void reverseDirectionalIntake(){
+        // if statement to revese depending on wether it is already inverted or not
+        if (directionalIntakeMotor.getInverted()){
+        directionalIntakeMotor.setInverted(false);
+        }
+        else{
+            directionalIntakeMotor.setInverted(true);
+        }
+    }
+
+    public void enableIntake(){
+        directionalIntakeMotor.set(1);// value, should probally change
+        intakeMotor.set(1); // temp value
+    }
+    
+    public void disableIntake(){
+        directionalIntakeMotor.stopMotor();
+        intakeMotor.stopMotor();
+    }
+
+    public double getDirectionalIntakeMotor(){
+        return directionalIntakeMotor.get();
+    }
+
+    public double getIntakeMotor(){
+        return intakeMotor.get();
+    }
+}
